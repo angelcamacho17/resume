c188acca35fca9c86677e7b2bd31ea234851a844
angelcamacho17 <angelcamacho1708@gmail.com>
2019-10-15 09:19:29 -0300

    Updates

---
+++ precache-manifest.883c3dfec40190665522a427fb46d7b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "18bea22a2a5d1e1d102c",
+    "url": "/resume/static/js/runtime~main.18bea22a.js"
+  },
+  {
+    "revision": "a0b1bbd325b5144217d5",
+    "url": "/resume/static/js/main.a0b1bbd3.chunk.js"
+  },
+  {
+    "revision": "2f4df31bb72e4cecd464",
+    "url": "/resume/static/js/1.2f4df31b.chunk.js"
+  },
+  {
+    "revision": "a0b1bbd325b5144217d5",
+    "url": "/resume/static/css/main.ff2b8aa4.chunk.css"
+  },
+  {
+    "revision": "ec10da6738312268b679897dee7e4f76",
+    "url": "/resume/index.html"
+  }
+];
