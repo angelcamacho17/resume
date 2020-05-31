6c5ba37af30748e6d0cc9d78ab0ef8466ec0a7f6
angelcamacho17 <angelcamacho1708@gmail.com>
2020-05-31 17:10:17 -0300

    Updates

---
+++ precache-manifest.8983aa257710f5e504319db6f8f16933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c244e1e8b0d7b497dc140625a5f6be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afe83f96283e40fbb8b3",
+    "url": "/static/css/main.e8986148.chunk.css"
+  },
+  {
+    "revision": "f2353a81a51c23f585fd",
+    "url": "/static/js/2.3a4fc8f6.chunk.js"
+  },
+  {
+    "revision": "afe83f96283e40fbb8b3",
+    "url": "/static/js/main.e6906e6a.chunk.js"
+  },
+  {
+    "revision": "79beb54b993ed6432830",
+    "url": "/static/js/runtime-main.4585df84.js"
+  }
+]);
